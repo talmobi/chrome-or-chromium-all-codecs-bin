ce66f36cbaf272bbd53756826d3dfbc9d0f207e1
Christian Talmo <talmo.christian@gmail.com>
2020-01-29 01:17:15 +0200

    feat: return chrome bin path if exists, else fallback to installed chromium bin path

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // give bin path to downloaded Chromium
 const fs = require( 'fs' )
 const path = require( 'path' )
-const binPath = fs.readFileSync( path.join( __dirname, 'bin-path.txt' ), 'utf8' )
+
+const chromeFinder = require( 'chrome-finder' )
+
+let binPath = ''
+try {
+  const p = chromeFinder()
+  binPath = p
+} catch ( err ) {
+  binPath = fs.readFileSync( path.join( __dirname, 'bin-path.txt' ), 'utf8' )
+}
+
 module.exports = function () {
   return path.relative( process.cwd(), binPath )
 }
